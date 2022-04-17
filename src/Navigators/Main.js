33b95a3371211d3bab8fee7d80d193eacac3844f
ahrenpradhan <ahrenpradhan@gmail.com>
2022-04-17 18:36:56 +0530

    update bottom navigation stage till 6:36 PM

--- src/Navigators/Main.js
+++ src/Navigators/Main.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { ExampleContainer } from '@/Containers'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator()
 
 // @refresh reset
 const MainNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline'
+          } else if (route.name === 'Invoices') {
+            iconName = focused ? 'file-tray-full' : 'file-tray-full-outline'
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'md-settings-sharp' : 'md-settings-outline'
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
       {/* <Tab.Screen
         name="Home"
         component={ExampleContainer}
@@ -19,18 +41,26 @@ const MainNavigator = () => {
       <Tab.Screen
         name="Home"
         component={ExampleContainer}
-        options={{
-          tabBarIconStyle: { display: 'none' },
-          // tabBarLabelPosition: 'beside-icon',
-        }}
+        // options={{
+        //   // tabBarIconStyle: { display: 'none' },
+        //   // tabBarLabelPosition: 'beside-icon',
+        // }}
       />
       <Tab.Screen
-        name="Home2"
+        name="Invoices"
         component={ExampleContainer}
-        options={{
-          tabBarIconStyle: { display: 'none' },
-          // tabBarLabelPosition: 'beside-icon',
-        }}
+        // options={{
+        //   // tabBarIconStyle: { display: 'none' },
+        //   // tabBarLabelPosition: 'beside-icon',
+        // }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={ExampleContainer}
+        // options={{
+        //   // tabBarIconStyle: { display: 'none' },
+        //   // tabBarLabelPosition: 'beside-icon',
+        // }}
       />
     </Tab.Navigator>
   )
